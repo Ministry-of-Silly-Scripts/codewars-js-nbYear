0b7af4976fed4bcc6ca21c38f051aeea97be6e71
Marcio Silva <marcionps@proton.me>
2023-07-02 19:01:22 -0300

    added canary test

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
 const { nbYear } = require("../src/index");
 
-test("test nbYear", () => {
-  expect(nbYear(0)).toBe(true);
+describe('the nbYear canary spec', () => {
+  it('shows the infrastructure works', () => {
+    expect(true).toBe(true);
+  });
 });
+
+// test("test nbYear", () => {
+//   expect(nbYear(0)).toBe(true);
+// });
